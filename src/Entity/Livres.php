84d9c70daa5d377552a4018efbc0dfa1d15aa454
malek <hamaissi.malak@gmail.com>
2023-04-03 10:57:32 +0100

    ajout de la table livres

---
+++ src/Entity/Livres.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivresRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivresRepository::class)]
+class Livres
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libellé = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $resumé = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibellé(): ?string
+    {
+        return $this->libellé;
+    }
+
+    public function setLibellé(string $libellé): self
+    {
+        $this->libellé = $libellé;
+
+        return $this;
+    }
+
+    public function getResumé(): ?string
+    {
+        return $this->resumé;
+    }
+
+    public function setResumé(string $resumé): self
+    {
+        $this->resumé = $resumé;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
